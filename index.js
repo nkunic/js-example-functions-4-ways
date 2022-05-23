c9b2c51464902f40ec7ae3600d3f1e769bd969da
nkunic <natasa.kunic@gmail.com>
2022-05-23 15:01:51 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,3 +2,34 @@
 import './style.css';
 
 // Write Javascript code!
+
+
+
+/* FUNCTION DECLARATION */
+var fd = document.getElementById('fd');
+function squareFd(x) {
+  return x * x;
+}
+fd.innerHTML = squareFd(2);
+
+
+/* FUNCTION EXPRESSION */
+var fe = document.getElementById('fe');
+const squareFe = function(x) {
+  return x * x;
+}
+fe.innerHTML = squareFe(3);
+
+
+/* ARROW FUNCTION EXPRESSION */
+var afe = document.getElementById('afe');
+const squareAfe = (x) => {
+  return x * x;
+}
+afe.innerHTML = squareAfe(4);
+
+
+/* CONCISE ARROW FUNCTION EXPRESSION */
+var cafe = document.getElementById('cafe');
+const squareCafe = x => x * x;
+cafe.innerHTML = squareCafe(5);
